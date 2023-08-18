2b61fa4ba3cc744deee1801e8ce2533b37bbcab1
Md Wasi Ul Kabir <mkabir3@uno.edu>
2023-08-18 11:06:13 -0500

    Update

--- download_largefiles.sh
+++ download_largefiles.sh
@@ -6,8 +6,9 @@ file="../Largefile.txt"
 while read -r line; do
     echo -e "$line\n"
     line2="${line:2}"
-    if [ ! -d "../$line2" ] 
+    if [ ! -d ../$line2 ] 
     then
+        
         filename=$(basename "$line")
         wget -nc https://www.cs.uno.edu/~mkabir3/$destinationFolder/$filename.tar.gz -P ./
         tar -xzvf $filename.tar.gz
