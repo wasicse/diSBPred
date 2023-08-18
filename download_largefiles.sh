1a2430b5abd0ee780317d76accab7620f0553e40
Md Wasi Ul Kabir <mkabir3@uno.edu>
2023-08-18 10:57:26 -0500

    Update download scripts.

--- download_largefiles.sh
+++ download_largefiles.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 destinationFolder="diSBPred"
-
-file="Largefile.txt"
+mkdir -p tempDownloads
+cd tempDownloads
+file="../Largefile.txt"
 while read -r line; do
     echo -e "$line\n"
     filename=$(basename "$line")
-    wget -nc https://www.cs.uno.edu/~mkabir3/$destinationFolder/$filename -P $line 
+    wget -nc https://www.cs.uno.edu/~mkabir3/$destinationFolder/$filename.tar.gz -P ./
+    tar -xzvf $filename.tar.gz
+    line2="${line:2}"
+    mv $line "../$line2" 
 done <$file
+
+rm Scripts
+rm Tools
